f9d93fb6e262ae365d23d520a8a32ebc1f337fd0
Mohamed Ehab <mtata3038@gmail.com>
2025-08-26 15:31:18 +0300

    push assets collection

--- src/routes/data.py
+++ src/routes/data.py
@@ -9,7 +9,9 @@ import logging
 from .schemes.data import ProcessRequest
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunkModel
-from models.db_schemes import DataChunk
+from models.AssetModel import AssetModel
+from models.db_schemes import DataChunk , Asset
+from models.enums.AssetEnum import AssetEnum
 
 
 logger = logging.getLogger('uvicorn.error')
@@ -53,11 +55,22 @@ async def upload_data(request: Request, project_id: str , file: UploadFile,
             status_code=status.HTTP_400_BAD_REQUEST,
             content={"Signal": responseSignal.FILE_UPLOAD_FAILED.value}
         )
+    
+    asset_model = await AssetModel.create_instance(db_client= request.app.db_client)
+
+    asset_resource = Asset(
+        asset_project_id= project.id,
+        asset_type= AssetEnum.FILE.value,
+        asset_name= file_id,
+        asset_size= os.path.getsize(file_path)
+    )
+
+    asset_record = await asset_model.create_asset(asset= asset_resource)
 
     return JSONResponse(
             content={
                 "Signal": responseSignal.FILE_UPLOAD_SUCCESS.value,
-                "file_id": file_id
+                "file_id": str(asset_record.id)
             }
         )
 
