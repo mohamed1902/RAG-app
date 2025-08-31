39a988a6d958d6fcbd16793c3c3ae01d4e3fa78f
Mohamed Ehab <mtata3038@gmail.com>
2025-08-31 12:21:41 +0300

    create the LLMEnums

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -10,3 +10,17 @@ class OpenAiEnums(Enum):
     SYSTEM = "system"
     USER = "user"
     ASSISTANT = "assistant"
+
+class CoHereEnums(Enum):
+
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT"
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+
+class DocumentTypeEnum(Enum):
+
+    DOCUMENT = "document"
+    QUERY = "query"
