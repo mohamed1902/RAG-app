975a37d748dd5ca3f0fa7302fba24146ef8e5a17
Mohamed Ehab <mtata3038@gmail.com>
2025-09-02 16:12:09 +0300

    update NLPController + NLP route

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -6,12 +6,13 @@ import json
 
 class NLPController(BaseController):
 
-    def __init__(self, vector_client, generation_client, embedding_client):
+    def __init__(self, vector_client, generation_client, embedding_client, template_parser):
         super().__init__()
 
         self.vectordb_client = vector_client
         self.generation_client = generation_client
         self.embedding_client = embedding_client
+        self.template_parser = template_parser
 
     def create_collection_name(self, project_id: str):
         return f"Collection: {project_id}".strip()
@@ -83,6 +84,47 @@ class NLPController(BaseController):
         if not results:
             return False
 
-        return json.loads(
-            json.dumps(results, default=lambda x:x.__dict__)
+        return results
+    
+    def answer_rag_question(self, project: Project, query: str, limit: int = 10):
+
+        answer, full_prompt, chat_history = None, None, None
+
+        # retrieve related documents
+        retrieved_documents = self.search_vector_db_collection(
+            project=project,
+            text=query,
+            limit=limit
         )
+
+        if not retrieved_documents or len(retrieved_documents) == 0:
+            return answer, full_prompt, chat_history
+        
+        # construct LLM prompt
+        system_prompt = self.template_parser.get("rag", "system_prompt")
+
+        documents_prompts = "\n".join([
+            self.template_parser.get("rag", "document_prompt", {
+                    "doc_num": idx + 1,
+                    "chunk_text": doc.text
+            })
+            for idx, doc in enumerate(retrieved_documents)
+        ])
+
+        footer_prompts = self.template_parser.get("rag", "footer_prompt")
+
+        chat_history = [
+            self.generation_client.construct_prompt(
+                prompt=system_prompt,
+                role=self.generation_client.enums.SYSTEM.value  
+            )
+        ]
+
+        full_prompt = "\n\n".join({documents_prompts, footer_prompts})
+
+        answer = self.generation_client.generate_text(
+            prompt=full_prompt,
+            chat_history=chat_history
+        )
+
+        return answer, full_prompt, chat_history
--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -118,7 +118,7 @@ async def get_project_index_info(request: Request, project_id: str):
 # ============== NLP Index Search ==============
 
 @nlp_router.post("/index/search/{project_id}")
-async def get_project_index_info(request: Request, project_id: str, search_request: SearchRequest):
+async def search_index(request: Request, project_id: str, search_request: SearchRequest):
 
     project_model = await ProjectModel.create_instance(
         db_client=request.app.db_client
@@ -131,7 +131,8 @@ async def get_project_index_info(request: Request, project_id: str, search_reque
     nlp_controller = NLPController(
         vector_client=request.app.vectordb_client,
         generation_client=request.app.generation_client,
-        embedding_client=request.app.embedding_client
+        embedding_client=request.app.embedding_client,
+        template_parser=request.app.template_parser
     )
 
     results = nlp_controller.search_vector_db_collection(
@@ -151,6 +152,50 @@ async def get_project_index_info(request: Request, project_id: str, search_reque
     return JSONResponse(
         content={
             "signal": responseSignal.VECTORDB_SEARCH_SUCCESS.value,
-            "results": results
+            "results": [ result.dict() for result in results ]
+        }
+    )
+
+
+# ============== NLP Index Answer ==============
+
+@nlp_router.post("/index/answer/{project_id}")
+async def answer_rag(request: Request, project_id: str, search_request: SearchRequest):
+
+    project_model = await ProjectModel.create_instance(
+        db_client=request.app.db_client
+    )
+
+    project = await project_model.get_project_or_create_one(
+        project_id=project_id
+    )
+
+    nlp_controller = NLPController(
+        vector_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client,
+        template_parser=request.app.template_parser
+    )
+
+    answer, full_prompt, chat_history = nlp_controller.answer_rag_question(
+        project=project,
+        query=search_request.text,
+        limit=search_request.limit
+    )
+
+    if not answer:
+        return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "signal": responseSignal.RAG_ANSWER_ERROR.value
+                }
+            )
+    
+    return JSONResponse(
+        content={
+            "signal": responseSignal.RAG_ANSWER_SUCCESS.value,
+            "answer": answer,
+            "full_prompt": full_prompt,
+            "chat_history": chat_history
         }
     )
